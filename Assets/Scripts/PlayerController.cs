8fcfd50eb53abe45b9c64ba15766fb2a105b7cf4
kristinleung <93747260+kristinleung@users.noreply.github.com>
2022-07-15 18:57:37 -0700

    Added barrel roll. Changed movement tilt to be around y-axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : Spaceship
 {
     Vector2 direction = Vector2.zero; // Holds direction of movement. 0,0 by default
 
+    bool performDodge = false;
+    bool isRight = false;
+    float dodgeRot = 0.0f;
 
     // Updates direction of movement
     public void InputMove(InputAction.CallbackContext context)
@@ -18,8 +21,26 @@ public class PlayerController : Spaceship
     new public void Update()
     {
         base.Update(); // Call the child class update
-        transform.rotation = Quaternion.identity; // Resets rotation (for tilt) to 0 before every move
-        Move(direction); // Move according to direction
+
+        if(performDodge == true) // Execute a dodge if performDodge tag is on
+        {
+            if (dodgeRot < 300) // Will rotate (roll) 300 degrees max, then reset to 0
+            {
+                Dodge(isRight);
+                dodgeRot += 15; // To animate, 15 degree rotation per frame
+            }
+            else
+            {
+                dodgeRot = 0.0f; // Completed one dodge/barrel roll, reset rotation back to 0
+                transform.rotation = Quaternion.identity;
+                performDodge = false;
+            }
+        }
+        else // Did not need to perform a dodge, check for movement
+        {
+            transform.rotation = Quaternion.identity; // Resets rotation (for tilt) to 0 before every move
+            Move(direction); // Move according to direction
+        }
 
     }
 
@@ -28,7 +49,8 @@ public class PlayerController : Spaceship
     {
         if (context.performed)
         {
-            Dodge(false); // Calls dodge function with isRight as false
+            performDodge = true; // Turn on tag so a dodge is performed on next call to Update
+            isRight = false; // Call Dodge function with isRight as false (left dodge)
         }
     }
 
@@ -37,7 +59,8 @@ public class PlayerController : Spaceship
     {
         if (context.performed)
         {
-            Dodge(true); // Calls dodge function with isRight as true
+            performDodge = true;
+            isRight = true;
         }
     }
 
