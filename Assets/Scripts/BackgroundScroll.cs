cd59b3236234774c883ebb61a10bff019591cea7
Nathan More <morenate2000@gmail.com>
2022-08-05 14:35:35 -0700

    Added adjustment to space background scroll speed.

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
+    [SerializeField]
+    private float scrollSpeedAdjustment = 1.0f;
+
     // Update is called once per frame
     void Update()
     {
@@ -11,7 +14,7 @@ public class BackgroundScroll : MonoBehaviour
         Material mat = mr.material;
         Vector2 offset = mat.mainTextureOffset;
         //regular scroll
-        offset.y += Time.deltaTime / 2f;
+        offset.y += (Time.deltaTime / 2f) * scrollSpeedAdjustment;
         mat.mainTextureOffset = offset;
     }
 }
